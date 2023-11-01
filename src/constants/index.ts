93bea486ffc4b8fcb4ca7ac47a766201af0b9765
Raj Parsaniya <132367009+rajparsaniya87@users.noreply.github.com>
2023-11-01 22:25:16 +0530

    Updated Constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,10 +6,10 @@ export const REPLAY_BUTTON_TEXT: string = "Replay";
 export const HINT_BUTTON_TEXT: string = "Hint";
 export const SOLVE_BUTTON_TEXT: string = "Solve";
 
-export const KEY_PRESSED_TOAST_LABEL: string = "'{0}' is already guessed";
+export const KEY_PRESSED_TOAST_LABEL: string = '"{0}" is already guessed';
 export const NOT_STARTED_TOAST_LABEL: string = "Game is not started";
 export const FINISHED_TOAST_LABEL: string = "Game is already finished";
-export const WON_TOAST_LABEL: string = "You Won";
-export const LOSE_TOAST_LABEL: string = "You Lose"
+export const WON_TOAST_LABEL: string = "You won the game";
+export const LOSE_TOAST_LABEL: string = "You lose the game";
 
 export const RANDOM_WORD_API: string = "https://random-word-api.herokuapp.com/all";
